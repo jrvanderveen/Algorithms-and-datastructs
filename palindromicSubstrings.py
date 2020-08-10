f2b2f5441d216ae86fb037a6cac3d5f19c4e2dfd
Johan van der Veen <wasjrvanderveen@gmail.com>
2020-08-10 13:45:03 -0700

    Small fix

--- palindromicSubstrings.py
+++ palindromicSubstrings.py
@@ -1,3 +1,12 @@
+def printArray(array):
+
+    for row in range(len(array)):
+        print()
+        for col in range(len(array[0])):
+            print(array[row][col], " ", end="")
+    print()
+
+
 class Solution(object):
     def countSubstrings(self, s):
         n = len(s)
@@ -9,14 +18,26 @@ class Solution(object):
                 if s[start] != s[end]:
                     dp[start][end] = 0
                 # if len < 3 and start == end then found
-                elif start-end+1 < 3:
+                elif end-start+1 < 3:
                     dp[start][end] = 1
                 # if len >= 3 and start == end then check if inner substring pal
                 else:
                     dp[start][end] = dp[start+1][end-1]
+
                 count += dp[start][end]
         return count
 
+
+try:
+    testCase = "fdsklf"
+    expected = 6
+    result = Solution().countSubstrings(testCase)
+    assert result == expected
+    print("Passed with expected result: ", expected)
+except AssertionError:
+    print("Testcast: ", testCase, " failed")
+    print("Expected: ", expected)
+    print("Output: ", result)
 
 try:
     testCase = "abba"
